cd077f9dc174283971f18d17c389f3a4684a3daf
ashmit-tech <153615365+ashmit-tech@users.noreply.github.com>
2024-04-21 18:04:29 +0530

    got basic knoledge(half knowlege) of react-router but can not understand full project

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import Contact, { loader as contactLoader } from "./routes/contact"; /////
+import Contact, {
+  loader as contactLoader,
+  action as contactAction,
+} from "./routes/contact"; /////
 import EditContact, { action as editAction } from "./routes/edit";
 import ErrorPage from "./error-page";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 import Root, {
   loader as rootLoader,
   action as rootAction,
@@ -19,9 +24,23 @@ const router = createBrowserRouter([
     action: rootAction, /////
     children: [
       {
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          /* the rest of the routes */
+        ],
+      },
+      {
         path: "contacts/:contactId",
         element: <Contact />,
         loader: contactLoader, //////
+        action: contactAction,
       },
       {
         path: "contacts/:contactId/edit",
@@ -29,6 +48,12 @@ const router = createBrowserRouter([
         loader: contactLoader,
         action: editAction,
       }, //////
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
+      },
+      { index: true, element: <Index /> },
     ],
   },
 ]);
